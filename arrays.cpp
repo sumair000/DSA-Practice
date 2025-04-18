3069b1aa0a959d8461f7e166f6d16276dcf35bdd
Sumair <sumair@apps-MacBook-Pro.local>
2025-04-18 22:28:54 +0500

    preperation

--- arrays.cpp
+++ arrays.cpp
@@ -38,6 +38,19 @@ void reverseArray(int arr[], int size){
     
 }
 
+void swapAlternat(int arr[], int size){
+
+    for (int i = 0; i < size; i+=2)
+    {
+        if (i+1 < size)
+        {
+            swap(arr[i],arr[i+1]);
+        }
+        
+    }
+    
+}
+
 void printArray(int arr[],int size){
     for (int i = 0; i < size ; i++)
     {
@@ -46,9 +59,38 @@ void printArray(int arr[],int size){
     cout<<endl;
 }
 
+int findUnique(int arr[], int size){
+
+    int ans = 0;
+
+    for(int i = 0; i < size; i++)  
+    {
+        ans = ans ^ arr[i];
+    }
+    
+    return ans;
+}
+
+
+int findDuplicate(int arr[], int size) {
+    int ans = 0;
+    // XOR all array elements
+    for (int i = 0; i < size; i++) {
+        ans ^= arr[i];
+    }
+    // XOR with 1 to n (where n = size-1)
+    for (int i = 1; i < size; i++) { // i < size = 7 → 1-6
+        ans ^= i;
+    }
+    return ans;
+}
+
+
 int main(){
 
-    int array[10] = {1,2,3,4,5,-4,7,8,0,-1};
+    // int array[11] = {1,2,5,-4,5,-4,0,8,0,1,2};
+
+    int arr[5] = {1,2,3,4,4};
     // int sum = sumOfArray(array, 5);
     // cout << "The sum of the array is: " << sum << endl;
 
@@ -63,8 +105,18 @@ int main(){
     //     cout<<"key is not present in the array";
     // }
 
-    reverseArray(array, 10);
-    printArray(array, 10);
+    // reverseArray(array, 10);
+
+    // swapAlternat(array,10);
+    
+    // int ans = findUnique(array, 11);
+    
+    int ans = findDuplicate(arr,5);
+    printArray(arr, 5);
+
+    cout << "duplicate number is: "<<ans;
+
+    // cout<<"unique value is : "<<ans;
     cout << endl;
     return 0;
 }
