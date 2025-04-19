eb27bfc33d56068de44cced57840f0ffa61e775b
Sumair <sumair@apps-MacBook-Pro.local>
2025-04-19 16:22:13 +0500

    peak mountain array

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -82,15 +82,37 @@ int lastOccurence(int arr[], int size, int key){
     return ans;
 }
 
+int peakMountain(int arr[], int size){
+
+    int s = 0;
+    int e = size -1;
+    while (s < e)  
+    {
+        int mid = s + (e -s)/2;
+        if (arr[mid] < arr[mid + 1])
+        {
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        
+    }
+    return s;
+    
+}
 int main(){
 
     int arr[14] = {1,3,4,4,4,4,4,4,4,4,5,7,8,11};
 
     // int ans = binartSearch(arr,14,4);
 
-    cout <<"first occurence is at index: "<< firstOccurence(arr, 14, 5);
-    cout <<endl;
-    cout <<"last occurence is at index: "<< lastOccurence(arr, 14, 5);
+    // cout <<"first occurence is at index: "<< firstOccurence(arr, 14, 5);
+    // cout <<endl;
+    // cout <<"last occurence is at index: "<< lastOccurence(arr, 14, 5);
+
+    int array[6] = {2,3,4,3,1,0};
+    cout << "peak element index is: "<< peakMountain(array, 6);
 
     cout <<endl;
 
