b61a2d3b587bbe84a01c45a27172d74b1f162ebf
Sumair <sumair@apps-MacBook-Pro.local>
2025-04-19 17:40:08 +0500

    find the target value in Rotated Sorted Array

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -101,6 +101,70 @@ int peakMountain(int arr[], int size){
     return s;
     
 }
+
+int findPivot(int arr[], int size){
+
+    int s = 0;
+    int e = size -1;
+    int mid = s + (e -s)/2;
+
+    while (s < e)  
+    {
+        if (arr[mid] > arr[0])
+        {
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e -s)/2;
+        
+    }
+    return s;
+    
+}
+
+int findSortedRotated(int arr[],int size, int target){
+
+    int s = 0;
+    int e = size -1;
+    int mid = s + (e - s)/2;
+
+    while (s <= e)
+    {
+        //check if mid is the target value
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        //check if the left half is sorted 
+        if (arr[s] <= arr[mid])
+        {
+            if (arr[s] <= target && target < arr[mid])
+            {
+                e = mid - 1;
+            }else{
+                s = mid + 1;
+            }
+            
+        }
+        else{
+            if (arr[mid] < target && target <= arr[e])
+            {
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+
+        }
+        
+        mid = s + (e - s)/ 2;
+    }
+    return -1;
+}
+
 int main(){
 
     int arr[14] = {1,3,4,4,4,4,4,4,4,4,5,7,8,11};
@@ -111,9 +175,14 @@ int main(){
     // cout <<endl;
     // cout <<"last occurence is at index: "<< lastOccurence(arr, 14, 5);
 
-    int array[6] = {2,3,4,3,1,0};
-    cout << "peak element index is: "<< peakMountain(array, 6);
+    int array[8] = {5,8,10,17,19,2,3,4};
+    // cout << "peak element index is: "<< peakMountain(array, 6);
+    // cout << "pivot element index is: "<< findPivot(array, 8);
+    cout <<endl;
 
+    cout << "the target value is at index: "<< findSortedRotated(array, 8, 2);
+
+    cout <<endl;
     cout <<endl;
 
 }
